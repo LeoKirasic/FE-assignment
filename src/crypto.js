bd5d48eb90e15fc53e2e35bcd5bbd5d752e08ae6
Leo Kirasic <kirasicleo@gmail.com>
2022-07-23 17:57:05 +0200

    Create populateCryptoTable function

--- src/crypto.js
+++ src/crypto.js
@@ -3,6 +3,47 @@ const getUSDExchangeRate = async () => {
   const data = await response.json();
   return data[0]['Srednji za devize'];
 };
+const populateCryptoTable = (data) => {
+  const cryptoTable = document.querySelector('.crypto-table');
+
+  for (let i = 1; i < cryptoTable.rows.length;) {
+    cryptoTable.deleteRow(i);
+  }
+  function addCell(tr, text) {
+    const td = tr.insertCell();
+    td.textContent = text;
+    return td;
+  }
+
+  const changeRowStyle = (id, style) => {
+    const row = document.querySelector(id);
+    row.classList.add(style);
+    setTimeout(() => {
+      row.classList.remove(style);
+    }, 1500);
+  };
+
+  const updateRow = () => {
+    if (previousData) {
+      for (let i = 0; i < previousData.length; i += 1) {
+        if (previousData[i].priceUsd < data[i].priceUsd) {
+          changeRowStyle(`.${previousData[i].id}`, 'positive');
+        } else {
+          changeRowStyle(`.${previousData[i].id}`, 'negative');
+        }
+      }
+    }
+  };
+  data.forEach((item) => {
+    const row = cryptoTable.insertRow();
+    row.classList.add(item.id);
+    addCell(row, item.rank);
+    addCell(row, item.id);
+    addCell(row, item.priceUsd);
+    addCell(row, item.priceUsd * exchangeRate);
+  });
+  updateRow();
+};
 const fetchAPIData = async () => {
   const response = await fetch('https://api.coincap.io/v2/assets?limit=10');
   const data = await response.json();
