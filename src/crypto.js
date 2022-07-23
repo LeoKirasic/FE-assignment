4c8e4efbb9430b3d4a463f2c343983d70b078c44
Leo Kirasic <kirasicleo@gmail.com>
2022-07-23 17:56:00 +0200

    Create getUSDExchangeRate function

--- src/crypto.js
+++ src/crypto.js
@@ -1,3 +1,8 @@
+const getUSDExchangeRate = async () => {
+  const response = await fetch('https://stormy-spire-77504.herokuapp.com/https://api.hnb.hr/tecajn/v1?valuta=USD&format=json', { mode: 'cors' });
+  const data = await response.json();
+  return data[0]['Srednji za devize'];
+};
 const fetchAPIData = async () => {
   const response = await fetch('https://api.coincap.io/v2/assets?limit=10');
   const data = await response.json();
